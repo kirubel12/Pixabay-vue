3f33856ba5a500ceee72ca6569af62b4df0eb24a
kirubel12 <kirubelhabte1991@gmail.com>
2023-01-04 17:51:56 +0300

    fetch all image

--- src/components/PixabayAllPhotos.vue
+++ src/components/PixabayAllPhotos.vue
@@ -5,91 +5,31 @@
                 <input type="text" class="input " style="max-width: 300px;">
                 <button class="button is-primary ml-3">Search</button>
             </div>
-    <div class="columns mt-5 box ">
-        <div class="column is-4 block">
-            <div class="card px-3 p-2">
-                <h4 class="has-text-centered title">Kirubel</h4>
-                <img src="https://pixabay.com/get/gaee700e470a220ef999aaa32b45a8577cdd8b62ab022f145e76fcd9095e772653fcd199c5b3fa77aea95318d5d324d26e44b2dc6de901c27c520c257dd95351d_1280.jpg" alt="">
-                <div class="card-content">
-                    <div class="media">
-                        <div class="media-left">
-                            <figure class="image is-64x64 ">
-                                <img src="https://cdn.pixabay.com/user/2018/05/28/23-31-09-916_250x250.jpg" alt="" class="is-rounded">
-                            </figure>
-                        </div>
-                        <div class="media-content">
-                            <p class="is-4 title">Kirubel</p>
-                            <p class="subtitle is-6">@kirubel</p>
-                        </div>
-                    </div>
-                    <div class="content">
-                        <p class="tag is-primary">Cat</p> <p class="tag is-primary">Catty</p>
-                    </div>
-                    <footer class="card-footer mt-5">
-                    <div class="mt-3">
-                        <p class="subtitle is-info">Likes - 100</p>
-                        <p class="subtitle">Download - 100</p>
-                        <p class="subtitle">Views - 100</p>
-                    </div>
-                    </footer>
-
-                </div>
-            </div>
-        </div>
+    <div class="columns mt-5 box" v-for="picture in PixabayPhotos">
         <div class="column is-4 ">
             <div class="card px-3 p-2">
-                <h4 class="has-text-centered title">Kirubel</h4>
-                <img src="https://pixabay.com/get/gaee700e470a220ef999aaa32b45a8577cdd8b62ab022f145e76fcd9095e772653fcd199c5b3fa77aea95318d5d324d26e44b2dc6de901c27c520c257dd95351d_1280.jpg" alt="">
+                <h4 class="has-text-centered title">{{ picture.type }}</h4>
+                <img :src="picture.largeImageURL" alt="">
                 <div class="card-content">
                     <div class="media">
                         <div class="media-left">
                             <figure class="image is-64x64 ">
-                                <img src="https://cdn.pixabay.com/user/2018/05/28/23-31-09-916_250x250.jpg" alt="" class="is-rounded">
+                                <img :src="picture.userImageURL" alt="" class="is-rounded">
                             </figure>
                         </div>
                         <div class="media-content">
-                            <p class="is-4 title">Kirubel</p>
-                            <p class="subtitle is-6">@kirubel</p>
+                            <p class="is-4 title">{{ picture.user }}</p>
+                            <p class="subtitle is-6">@ {{ picture.user_id }}</p>
                         </div>
                     </div>
-                    <div class="content">
-                        <p class="tag is-primary">Cat</p> <p class="tag is-primary">Catty</p>
+                    <div class="content" v-for="tag in picture.tags">
+                    
                     </div>
                     <footer class="card-footer mt-5">
                     <div class="mt-3">
-                        <p class="subtitle is-info">Likes - 100</p>
-                        <p class="subtitle">Download - 100</p>
-                        <p class="subtitle">Views - 100</p>
-                    </div>
-                    </footer>
-
-                </div>
-            </div>
-        </div>
-        <div class="column is-4 ">
-            <div class="card px-3 p-2">
-                <h4 class="has-text-centered title">Kirubel</h4>
-                <img src="https://pixabay.com/get/gaee700e470a220ef999aaa32b45a8577cdd8b62ab022f145e76fcd9095e772653fcd199c5b3fa77aea95318d5d324d26e44b2dc6de901c27c520c257dd95351d_1280.jpg" alt="">
-                <div class="card-content">
-                    <div class="media">
-                        <div class="media-left">
-                            <figure class="image is-64x64 ">
-                                <img src="https://cdn.pixabay.com/user/2018/05/28/23-31-09-916_250x250.jpg" alt="" class="is-rounded">
-                            </figure>
-                        </div>
-                        <div class="media-content">
-                            <p class="is-4 title">Kirubel</p>
-                            <p class="subtitle is-6">@kirubel</p>
-                        </div>
-                    </div>
-                    <div class="content">
-                        <p class="tag is-primary">Cat</p> <p class="tag is-primary">Catty</p>
-                    </div>
-                    <footer class="card-footer mt-5">
-                    <div class="mt-3">
-                        <p class="subtitle is-info">Likes - 100</p>
-                        <p class="subtitle">Download - 100</p>
-                        <p class="subtitle">Views - 100</p>
+                        <p class="subtitle is-info">Likes - {{ picture.likes }}</p>
+                        <p class="subtitle">Download - {{ picture.downloads }}</p>
+                        <p class="subtitle">Views - {{ picture.views }}</p>
                     </div>
                     </footer>
 
@@ -109,8 +49,8 @@ async function getData(){
     await fetch(url)
     .then((res) => res.json())
     .then((photos) => {
-        photos.value = PixabayPhotos
-        console.log(photos.hits)
+        PixabayPhotos.value = photos.hits
+        console.log(photos)
     })
 }
 getData()
